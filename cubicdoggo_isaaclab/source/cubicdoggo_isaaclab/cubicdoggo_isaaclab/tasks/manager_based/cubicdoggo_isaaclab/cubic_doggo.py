e49276d357a7013f8f662dce367b3e786b3b2ae5
SphericalCowww <tinglin194@gmail.com>
2026-03-14 20:50:38 +0100

    - initial files for IsaacLab

---
+++ cubicdoggo_isaaclab/source/cubicdoggo_isaaclab/cubicdoggo_isaaclab/tasks/manager_based/cubicdoggo_isaaclab/cubic_doggo.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Reference:
+* https://github.com/ANYbotics/anymal_b_simple_description
+* https://github.com/ANYbotics/anymal_c_simple_description
+* https://github.com/ANYbotics/anymal_d_simple_description
+
+"""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ActuatorNetLSTMCfg, DCMotorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.sensors import RayCasterCfg
+from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+from isaaclab_assets.sensors.velodyne import VELODYNE_VLP_16_RAYCASTER_CFG
+
+##
+# Configuration - Actuators.
+##
+
+ANYDRIVE_3_SIMPLE_ACTUATOR_CFG = DCMotorCfg(
+    joint_names_expr=[".*HAA", ".*HFE", ".*KFE"],
+    saturation_effort=120.0,
+    effort_limit=80.0,
+    velocity_limit=7.5,
+    stiffness={".*": 40.0},
+    damping={".*": 5.0},
+)
+"""Configuration for ANYdrive 3.x with DC actuator model."""
+
+
+ANYDRIVE_3_LSTM_ACTUATOR_CFG = ActuatorNetLSTMCfg(
+    joint_names_expr=[".*HAA", ".*HFE", ".*KFE"],
+    network_file=f"{ISAACLAB_NUCLEUS_DIR}/ActuatorNets/ANYbotics/anydrive_3_lstm_jit.pt",
+    saturation_effort=120.0,
+    effort_limit=80.0,
+    velocity_limit=7.5,
+)
+"""Configuration for ANYdrive 3.0 (used on ANYmal-C) with LSTM actuator model."""
+
+
+##
+# Configuration - Articulation.
+##
+
+CUBIC_DOGGO_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=f"/home/cubicdoggo/Documents/CubicDoggo/cubicdoggo_isaaclab/usd/cubic_doggo.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=4, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.02, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.6),
+        joint_pos={
+            ".*HAA": 0.0,  # all HAA
+            ".*F_HFE": 0.4,  # both front HFE
+            ".*H_HFE": -0.4,  # both hind HFE
+            ".*F_KFE": -0.8,  # both front KFE
+            ".*H_KFE": 0.8,  # both hind KFE
+        },
+    ),
+    actuators={"legs": ANYDRIVE_3_LSTM_ACTUATOR_CFG},
+    soft_joint_pos_limit_factor=0.95,
+)
+)
+
+
+##
+# Configuration - Sensors.
+##
+
+#CUBIC_DOGGO_LIDAR_CFG = VELODYNE_VLP_16_RAYCASTER_CFG.replace(
+#    offset=RayCasterCfg.OffsetCfg(pos=(-0.310, 0.000, 0.159), rot=(0.0, 0.0, 0.0, 1.0))
+#)
+#"""Configuration for the Velodyne VLP-16 sensor mounted on the ANYmal robot's base."""
+
+
+
+
+
+
+
